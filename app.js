d48a604d8625f8127200e7111ebe216e745bb504
theADAMJR <adamjf123@gmail.com>
2020-07-31 19:09:19 +0100

    avg - 227.42ms

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 const api = require('./wrapper/api');
 const auth = require('./wrapper/auth')
-const user = require('./user.json');
+const { account, desiredUsername } = require('./user.json');
 
 (async() => {
-  const uuid = await api.getUUID(user.username);
-  console.log(`${user.username} UUID: ${uuid}`);
-
-  const bearerToken = await auth.authenticate(user);
-  console.log(bearerToken);
+  const { accessToken, selectedProfile } = await auth.authenticate(account);
+  if (!accessToken)
+    return console.log(`RESULT: Invalid username or password.`);
+  
+  await changeUsername(account.bearerToken, selectedProfile);
 })();
+
+async function changeUsername(bearerToken, profile) {    
+  const divider = '------------------------------';
+  console.log(`CHANGING USERNAME\n` +
+    `Old: ${profile.name}\n` +
+    `New: ${desiredUsername}\n` +
+    divider);
+
+  console.time('changeUsername');
+
+  const attempt = await api.changeUsername(account.password, {
+    bearerToken,
+    desiredUsername,
+    uuid: profile.id
+  });
+
+  console.timeEnd('changeUsername');
+  // 
+
+  if (attempt.error) {
+    console.log(`ERROR: ${attempt.errorMessage}.`);
+    console.log(`RESULT: Mission Failed. We'll get em next time.`);
+  }
+}
